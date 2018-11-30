da4b9af345a75532673d43e1c23366e879c9af40
Jinhda Douang <jinhda.douangphrachanh@epitech.eu>
2018-11-30 15:23:50 +0100

    removed www from git and updated footervue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,42 +1,53 @@
 <template>
-    <ion-footer>
 
+    <footer>
         <!--https://ionicframework.com/docs/api/components/fab/FabList/-->
         <!-- fab placed to the bottom end -->
         <ion-fab vertical="bottom" horizontal="end" slot="fixed">
-            <ion-fab-button color="dark">
+            <ion-fab-button class="footer-button">
                 <ion-icon name="arrow-dropleft"></ion-icon>
             </ion-fab-button>
 
-
-            <ion-fab-list side="start" class="fab-list-side-start hydrated">
-                <ion-fab-button class="fab-button-in-list hydrated">
-                    <ion-icon name="logo-facebook" role="img" class="hydrated" aria-label="logo facebook"></ion-icon>
+            <ion-fab-list side="start">
+                <ion-fab-button>
+                    <ion-icon name="logo-facebook"></ion-icon>
                 </ion-fab-button>
 
-                <ion-fab-button class="fab-button-in-list hydrated">
-                    <ion-icon name="logo-twitter" role="img" class="hydrated" aria-label="logo twitter"></ion-icon>
+                <ion-fab-button>
+                    <ion-icon name="logo-twitter"></ion-icon>
                 </ion-fab-button>
 
-                <ion-fab-button class="fab-button-in-list hydrated">
-                    <ion-icon name="logo-vimeo" role="img" class="hydrated" aria-label="logo vimeo"></ion-icon>
+                <ion-fab-button>
+                    <ion-icon name="logo-vimeo"></ion-icon>
                 </ion-fab-button>
 
-                <ion-fab-button class="fab-button-in-list hydrated">
-                    <ion-icon name="logo-instagram" role="img" class="hydrated" aria-label="logo instagram"></ion-icon>
+                <ion-fab-button>
+                    <ion-icon name="logo-instagram"></ion-icon>
                 </ion-fab-button>
 
             </ion-fab-list>
         </ion-fab>
-    </ion-footer>
-
+    </footer>
 </template>
 
 
 <script>
 
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
 </script>
 
 <style>
-
+    .footer-button {
+        --background: black;
+        --background-activated: black;
+    }
 </style>
