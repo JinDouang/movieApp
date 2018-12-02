c4a5ace6cb1b54e39d85a5a77f65a130e7ec5f67
Jinhda Douang <jinhda.douangphrachanh@epitech.eu>
2018-12-02 16:34:18 +0100

    card design added to popular

--- src/pages/Popular.vue
+++ src/pages/Popular.vue
@@ -1,16 +1,33 @@
 <template>
     <ion-page class="show-page">
         <ion-content class="content">
-            <ion-card class="card">
-                <ion-card-header>
-                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-                    <ion-card-title>Card Title</ion-card-title>
-                </ion-card-header>
+            <ion-grid>
+                <ion-row>
 
-                <ion-card-content>
-                    This is Popular cc
-                </ion-card-content>
-            </ion-card>
+                    <ion-col class="col">
+                        <ion-card class="movie-info">
+                            <ion-card-title  class="card-title">
+                                Test
+                            </ion-card-title>
+                            <img src="https://placeimg.com/500/750/any"/>
+                            <div class="card-subtitle">
+                                <ion-row>
+                                    <ion-col>
+                                        <ion-icon name="star"></ion-icon>
+                                        <div>9.4</div>
+                                    </ion-col>
+                                    <ion-col>
+                                        <ion-icon name="calendar"></ion-icon>
+                                        <div>1995</div>
+                                    </ion-col>
+                                </ion-row>
+                            </div>
+                        </ion-card>
+                    </ion-col>
+
+
+                </ion-row>
+            </ion-grid>
         </ion-content>
     </ion-page>
 </template>
@@ -21,14 +38,30 @@
 </script>
 
 <style>
-    .card {
-        --background: white;
-    }
-
     .content {
         --offset-top: 0px !important;
         --offset-bottom: 544px !important;
         --background: #22242800;
     }
 
+
+
+    .card-title{
+        font-size: 1.5em;
+        width: 100%;
+        font-weight: bold;
+        background: #0000004d;
+    }
+
+    .card-subtitle {
+        font-size: 1.0em;
+        position: absolute;
+        bottom: 10px;
+        width: 100%;
+        color:#ecf0f1;
+    }
+
+    .col {
+        flex: 0 0 50%;
+    }
 </style>
