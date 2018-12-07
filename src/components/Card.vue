84512ebb78ae5a7929b73d021684232c33ad58a4
Jinhda Douang <jinhda.douangphrachanh@epitech.eu>
2018-12-07 13:55:26 +0100

    created card component

---
+++ src/components/Card.vue
@@ -0,0 +1,59 @@
+<template>
+    <ion-grid>
+        <ion-row>
+            <ion-col class="col">
+                <ion-card class="movie-info">
+                    <ion-card-title class="card-title">{{ title }}</ion-card-title>
+                    <img src="https://placeimg.com/500/750/any" />
+                    <div class="card-subtitle">
+                        <ion-row>
+                            <ion-col>
+                                <ion-icon name="star"></ion-icon>
+                                <div class="description">9.4</div>
+                            </ion-col>
+                            <ion-col>
+                                <ion-icon name="calendar"></ion-icon>
+                                <div class="description">1995</div>
+                            </ion-col>
+                        </ion-row>
+                    </div>
+                </ion-card>
+            </ion-col>
+        </ion-row>
+    </ion-grid>
+</template>
+
+
+<script>
+  export default {
+    props: ['title'],
+
+  }
+</script>
+
+<style>
+    .card-title {
+        font-size: 1.5em;
+        width: 100%;
+        font-weight: bold;
+        background: #f4f5f812;
+        color: white;
+    }
+
+    .card-subtitle {
+        font-size: 1.0em;
+        position: absolute;
+        bottom: 10px;
+        width: 100%;
+        color: #ecf0f1;
+    }
+
+    .col {
+        flex: 0 0 50%;
+    }
+
+    .description {
+        font-weight: bolder;
+        color: white;
+    }
+</style>
--- src/pages/Popular.vue
+++ src/pages/Popular.vue
@@ -1,40 +1,19 @@
 <template>
+
     <ion-content class="content">
-
-        <ion-grid>
-            <ion-row>
-
-                <ion-col class="col">
-                    <ion-card class="movie-info">
-                        <ion-card-title class="card-title">
-                            Test
-                        </ion-card-title>
-                        <img src="https://placeimg.com/500/750/any" />
-                        <div class="card-subtitle">
-                            <ion-row>
-                                <ion-col>
-                                    <ion-icon name="star"></ion-icon>
-                                    <div>9.4</div>
-                                </ion-col>
-                                <ion-col>
-                                    <ion-icon name="calendar"></ion-icon>
-                                    <div>1995</div>
-                                </ion-col>
-                            </ion-row>
-                        </div>
-                    </ion-card>
-                </ion-col>
-            </ion-row>
-        </ion-grid>
-
+        <card title="coucou"></card>
     </ion-content>
 </template>
 
 
 <script>
   import movieService from "@/service/movieService";
+  import Card from '../components/Card.vue';
 
   export default {
+    components: {
+      'card': Card,
+    },
     created () {
       movieService.getPopularMovies()
         .then(response => {
@@ -52,24 +31,5 @@
     .content {
         position: absolute;
         --background: #22242800;
-    }
-
-    .card-title {
-        font-size: 1.5em;
-        width: 100%;
-        font-weight: bold;
-        background: #0000004d;
-    }
-
-    .card-subtitle {
-        font-size: 1.0em;
-        position: absolute;
-        bottom: 10px;
-        width: 100%;
-        color: #ecf0f1;
-    }
-
-    .col {
-        flex: 0 0 50%;
     }
 </style>
