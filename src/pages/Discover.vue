e7a9eecd2a712cb3ab090812a40ec97b23446e1b
Jinhda Douang <jinhda.douangphrachanh@epitech.eu>
2018-12-14 03:51:52 +0100

    discover wip

--- src/pages/Discover.vue
+++ src/pages/Discover.vue
@@ -1,23 +1,64 @@
 <template>
-    <ion-page class="show-page">
         <ion-content class="content">
-            <ion-card class="card">
-                <ion-card-header>
-                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-                    <ion-card-title>Card Title</ion-card-title>
-                </ion-card-header>
 
-                <ion-card-content>
-                    This is Discover
-                </ion-card-content>
-            </ion-card>
+            <ion-searchbar type></ion-searchbar>
+            <ion-grid>
+                <ion-row>
+                    <card v-for="(item, index) in items"
+                          :key="index"
+                          :img="item.poster_path"
+                          :star="item.vote_average"
+                          :date="item.release_date">
+                    </card>
+                </ion-row>
+                <ion-infinite-scroll @ionInfinite="loadPage($event)">
+                    <ion-infinite-scroll-content
+                            loadingSpinner="bubbles"
+                            loadingText="Loading more data...">
+                    </ion-infinite-scroll-content>
+                </ion-infinite-scroll>
+            </ion-grid>
         </ion-content>
-    </ion-page>
 </template>
 
 
 <script>
-  export default {}
+  import Card from '../components/Card.vue';
+  import movieService from "@/service/movieService";
+
+  export default {
+    components: {
+      'card': Card,
+    },
+    data () {
+      return {
+        items: [],
+        page: 1
+      }
+    },
+    created () {
+      this.getUpcomingMovies(this.page);
+    },
+    methods: {
+      loadPage($event) {
+        this.page = this.page + 1;
+        this.getUpcomingMovies(this.page);
+        $event.target.complete();
+      },
+
+      getUpcomingMovies(page) {
+        movieService.getUpcomingMovies(page)
+          .then(response => {
+            for (let i = 0; i < response.data.results.length; i++) {
+              this.items.push(response.data.results[i]);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+    },
+  }
 </script>
 
 <style>
